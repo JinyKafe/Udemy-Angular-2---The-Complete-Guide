85ab0c008df39cfba7bc2c668b32cab12d0e26f1
jankotek <jankotek@atlas.cz>
2016-12-15 12:29:23 +0100

    S1L12: alterenative to templateUrl and syteleUrls

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
+@Component({ // each compnene must have decorator
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html', // each component must have exactly one html template
+  template: `
+      <h1>{{title}}</h1> 
+  `, // alternative to templateUrl
+  // styleUrls: ['./app.component.css'] // and may have (must not) multiple css
+  styles: [`
+        h1{ color:red; }
+  `] // alternative to styleUrls
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'I changed it!';
 }
